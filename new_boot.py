f304451291b4866c20ade5b5a87070965cf58a6d
Shmood00 <43577426+Shmood00@users.noreply.github.com>
2025-12-29 19:23:32 -0500

    Update new_boot.py

--- new_boot.py
+++ new_boot.py
@@ -5,48 +5,59 @@ import time
 FLAG_FILE = ".reset_flag"
 WIFI_FILE = "wifi.dat"
 OTA_FLAG = ".ota_running"
+
 led = machine.Pin(33, machine.Pin.OUT)
 
-# 1. Check for OTA bypass FIRST
+# --- OTA BOOT ---
 if OTA_FLAG in os.listdir():
-    print("[System] Post-OTA Boot: Keeping WiFi credentials.")
+    print("[System] Post-OTA boot.")
     try:
         os.remove(OTA_FLAG)
         if FLAG_FILE in os.listdir():
             os.remove(FLAG_FILE)
     except:
         pass
-    # No wiping logic here, just proceed to connect
 
-# 2. Check for "Double Reset" if no OTA bypass is present
+    # 🔴 Allow WiFi stack to stabilize
+    time.sleep(2)
+
+# --- DOUBLE RESET ---
 elif FLAG_FILE in os.listdir():
-    print("!!! Double Reset Detected: Wiping WiFi Credentials !!!")
-    
-    # Rapid Blink feedback
+    print("[System] Double reset detected. Wiping WiFi.")
+
     for _ in range(15):
-        led.value(0) 
+        led.value(0)
         time.sleep_ms(50)
-        led.value(1) 
+        led.value(1)
         time.sleep_ms(50)
 
-    try: os.remove(WIFI_FILE)
-    except: pass
-    
-    try: os.remove(FLAG_FILE)
-    except: pass
-    
+    try:
+        os.remove(WIFI_FILE)
+    except:
+        pass
+
+    try:
+        os.remove(FLAG_FILE)
+    except:
+        pass
+
     machine.reset()
 
+# --- NORMAL BOOT ---
 else:
-    # 3. Normal Boot: Create the flag if we have WiFi credentials.
     if WIFI_FILE in os.listdir():
-        print("Setting Reset Flag...")
+        print("[System] Setting reset flag.")
         with open(FLAG_FILE, "w") as f:
             f.write("1")
     else:
-        print("No WiFi file found. Setup Mode.")
+        print("[System] No WiFi config found.")
 
-# 4. Connect to WiFi
+# --- WIFI CONNECT ---
 from wifimanager import WifiManager
-wm = WifiManager(ssid='Family Ornament', password='', reboot=True, debug=True)
+wm = WifiManager(
+    ssid="Family Ornament",
+    password="",
+    reboot=True,
+    debug=True
+)
 wm.connect()
